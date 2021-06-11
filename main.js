e8911a813b70f740c8b24916baf9eab43fa5da60
Al9xOleksandr <59178982+Al9xOleksandr@users.noreply.github.com>
2021-06-12 01:01:05 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,51 @@
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Стать волонтером'], 'text',['tomato','rebeccapurple','lightblue']);
 
-    const backToTopButton = document.querySelector("#back-to-top-btn");
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-    window.addEventListener("scroll", scrollFunction);
-    
-    function scrollFunction() {
-      if (window.pageYOffset > 300) { // Show backToTopButton
-        if(!backToTopButton.classList.contains("btnEntrance")) {
-          backToTopButton.classList.remove("btnExit");
-          backToTopButton.classList.add("btnEntrance");
-          backToTopButton.style.display = "block";
-        }
-      }
-      else { // Hide backToTopButton
-        if(backToTopButton.classList.contains("btnEntrance")) {
-          backToTopButton.classList.remove("btnEntrance");
-          backToTopButton.classList.add("btnExit");
-          setTimeout(function() {
-            backToTopButton.style.display = "none";
-          }, 250);
-        }
-      }
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
     }
-    
-    backToTopButton.addEventListener("click", smoothScrollBackToTop);
-    
-    // function backToTop() {
-    //   window.scrollTo(0, 0);
-    // }
-    
-    function smoothScrollBackToTop() {
-      const targetPosition = 0;
-      const startPosition = window.pageYOffset;
-      const distance = targetPosition - startPosition;
-      const duration = 750;
-      let start = null;
-      
-      window.requestAnimationFrame(step);
-    
-      function step(timestamp) {
-        if (!start) start = timestamp;
-        const progress = timestamp - start;
-        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-        if (progress < duration) window.requestAnimationFrame(step);
-      }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
     }
-    
-    function easeInOutCubic(t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t*t + b;
-        t -= 2;
-        return c/2*(t*t*t + 2) + b;
-    };
+  }, 400)
+}
