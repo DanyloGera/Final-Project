8b993072231f458301191940247da930182fd1d2
nika <58823371+nika563@users.noreply.github.com>
2021-06-12 11:59:07 +0300

    Add files via upload

---
+++ game/game.js
@@ -0,0 +1,26 @@
+const inputCalc = document.querySelector('.accont'),
+      resultCalc = document.querySelector('.result');
+function input(nam){
+	inputCalc.value = inputCalc.value + nam;
+}
+function result() {
+	if( eval(inputCalc.value) == undefined ){
+		resultCalc.value = '0,00' ;
+	   inputCalc.value = '0,00' ;
+	}
+	if ( eval(inputCalc.value) == Infinity){
+		resultCalc.value = '0,00' ;
+	   inputCalc.value = 'Поздравляю!!! Вы сломали калькулятор.' ; //На ноль не делиться!!! 
+	}
+	resultCalc.value = eval(inputCalc.value);
+	inputCalc.value = eval(inputCalc.value);
+}
+
+function backspace() {
+	inputCalc.value = inputCalc.value.substring( 0, inputCalc.value.length-1)
+}
+
+function reset() {
+	resultCalc.value = '0,00' ;
+	inputCalc.value = '' ;
+}
