c1e95e785aacdb3bbda29be306246304514103be
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-06-25 17:33:32 +0300

    final correct

--- Game-ua/game-3ua/game.js
+++ Game-ua/game-3ua/game.js
@@ -10,7 +10,7 @@ var text;
 var winningMessage;
 var won = false;
 var currentScore =610;
-var winningScore = 0;
+var winningScore = 650;
 
 // add collectable items to the game
 function addItems() {
@@ -64,16 +64,16 @@ function createBadge() {
 function itemHandler(player, item) {
   item.kill();
    if (item.key === 'coin1') {
-     currentScore = currentScore - 152.5;
+     currentScore = currentScore + 10;
      game.add.sprite(275, 510, 'fruitbasket');
   }else if (item.key === 'coin2') {
-     currentScore = currentScore - 152.5;
+     currentScore = currentScore + 10;
      game.add.sprite(480, 510, 'butterbasket');
   } else if (item.key === 'coin3') {
-     currentScore = currentScore - 152.5;
+     currentScore = currentScore + 10;
      game.add.sprite(180, 250, 'giftbasket');
   }else if (item.key === 'coin4') {
-     currentScore = currentScore - 152.5;
+     currentScore = currentScore + 10;
      game.add.sprite(385, 250, 'meatbasket');
   }
   if (currentScore === winningScore) {
@@ -85,7 +85,7 @@ function itemHandler(player, item) {
 function badgeHandler(player, badge) {
   badge.kill();
   won = true;
-  window.location.href="https://danylogera.github.io/Titles-ua/";
+  window.location.href="../Titles-ua/index.html";
 }
 
 // setup game when the web page loads
@@ -94,12 +94,16 @@ window.onload = function () {
 
   // before the game begins
   function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    game.scale.setMinMax(700, 500, 900, 700);
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
 
-    game.load.image('background', 'background.png');
+    game.load.image('background', './img/background.png');
 
     //Load images
-    game.load.image('platform1', 'platform.png');
-    game.load.image('platform2', 'platform2.png');
+    game.load.image('platform1', './img/platform.png');
+    game.load.image('platform2', './img/platform2.png');
     //stairs
     game.load.image('step1', 'stairs2/step1.png');
     game.load.image('step2', 'stairs2/step2.png');
@@ -111,10 +115,10 @@ window.onload = function () {
 
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png',  48, 62 );
-    game.load.spritesheet('grandma', 'grandma.png', { frameWidth: 60, frameHeight: 75 });
-    game.load.spritesheet('grandpa', 'grandpa.png', { frameWidth: 60, frameHeight: 75 });
-    game.load.spritesheet('grandma2', 'grandma2.png', { frameWidth: 60, frameHeight: 75 });
-    game.load.spritesheet('grandpa2', 'grandpa2.png', { frameWidth: 60, frameHeight: 75 });
+    game.load.spritesheet('grandma', './img/grandma.png', { frameWidth: 60, frameHeight: 75 });
+    game.load.spritesheet('grandpa', './img/grandpa.png', { frameWidth: 60, frameHeight: 75 });
+    game.load.spritesheet('grandma2', './img/grandma2.png', { frameWidth: 60, frameHeight: 75 });
+    game.load.spritesheet('grandpa2', './img/grandpa2.png', { frameWidth: 60, frameHeight: 75 });
 
     game.load.spritesheet('coin1', 'coin.png', 36, 44);
     game.load.spritesheet('coin2', 'coin.png', 36, 44);
@@ -122,10 +126,10 @@ window.onload = function () {
     game.load.spritesheet('coin4', 'coin.png', 36, 44);
 
 
-    game.load.spritesheet('fruitbasket', 'fruitbasket.png', { frameWidth: 40, frameHeight: 40 });
-    game.load.spritesheet('meatbasket', 'meatbasket.png', { frameWidth: 40, frameHeight: 40 });
-    game.load.spritesheet('giftbasket', 'giftbasket.png', { frameWidth: 40, frameHeight: 40 });
-    game.load.spritesheet('butterbasket', 'butterbasket.png', { frameWidth: 40, frameHeight: 40 });
+    game.load.spritesheet('fruitbasket', './img/fruitbasket.png', { frameWidth: 40, frameHeight: 40 });
+    game.load.spritesheet('meatbasket', './img/meatbasket.png', { frameWidth: 40, frameHeight: 40 });
+    game.load.spritesheet('giftbasket', './img/giftbasket.png', { frameWidth: 40, frameHeight: 40 });
+    game.load.spritesheet('butterbasket', './img/butterbasket.png', { frameWidth: 40, frameHeight: 40 });
 
     game.load.spritesheet('gamemachine', 'gamemachine.png', { frameWidth: 49, frameHeight: 85 });
 
@@ -148,7 +152,7 @@ window.onload = function () {
 
   // initial game set up
   function create() {
-    // alert("Прочитайте завдання, закрийте його на хрестик і грайте!");
+
     timer1();
     this.add.image(0, 0, 'background');
     game.add.sprite(266, 467, 'grandma');
